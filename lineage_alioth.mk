0c7823b04e8e2cb474e0919253fd227ec20b3345
Rve27 <rve27github@gmail.com>
2025-11-24 23:18:06 +0700

    alioth: Adapt to AxionAOSP
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -9,14 +9,70 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Inherit Camera-related flags
-TARGET_USES_MIUI_CAMERA := true
-TARGET_INCLUDES_MIUI_CAMERA := true
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
+
+# Maintainer
+AXION_MAINTAINER := Rve
+
+# Blur effect
+TARGET_ENABLE_BLUR := true
+
+# Camera
+TARGET_USES_MIUI_CAMERA := true
+TARGET_INCLUDES_MIUI_CAMERA := true
+AXION_CAMERA_REAR_INFO := 48,8,5
+AXION_CAMERA_FRONT_INFO := 20
+
+# CPU
+AXION_PROCESSOR := Snapdragon_870_5G
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+PERF_ANIM_OVERRIDE := false
+
+# GPU
+GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/gpu_available_frequencies
+GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/min_clock_mhz
+
+# High Brightness Mode (HBM)
+HBM_SUPPORTED := true
+HBM_NODE := /sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm
+
+# Flashlight strength
+TORCH_STR_SUPPORTED := false
+
+# doze fix
+# for devices with doze/sensor related issues 
+TARGET_NEEDS_DOZE_FIX := false
+
+# Charging
+BYPASS_CHARGE_SUPPORTED := true
+
+# [experimental] userspace simple lmk trial version 
+# of https://github.com/kerneltoast/android_kernel_google_gs201/blob/16.0.0-sultan/drivers/android/simple_lmk.c ©kerneltoast
+# Targets devices with prebuilt kernels or uslmk support across kernels
+TARGET_USES_USLMK := false
+
+# do not enable TARGET_IS_LOW_RAM if your device ram is greater than 4gb
+# else OOM will most likely occur on operations where applications and camera can fill heap limit
+# e.g uploading video/media on apps with camera preview
+# this is a mitigation targets legacy devices 4gb below
+# Using quicken is a trade-off: here we trade clean pages for dirty pages,
+# extra cpu and battery. That's because the quicken files will be jit-ed in all
+# the processes that load of shared apk and the code cache is not shared.
+# Some notable apps that will be affected by this are gms and chrome.
+TARGET_IS_LOW_RAM := false
+
+# LineageOS apps
+ifneq ($(WITH_GMS),true)
+TARGET_INCLUDES_LOS_PREBUILTS := true
+endif
+
+# BCR
+TARGET_PREBUILT_BCR := false
 
 PRODUCT_NAME := lineage_alioth
 PRODUCT_DEVICE := alioth
